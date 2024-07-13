ab7601885598a93d06435aefd330651cd80d0bdf
fanbayy <fandi.bayu110@gmail.com>
2024-07-14 02:10:08 +0700

    fixing with callback animation and added static img to handle first

--- src/main.js
+++ src/main.js
@@ -69,3 +69,79 @@ document.addEventListener("DOMContentLoaded", () => {
     lottieContainer.classList.remove("hidden");
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const staticImageId = document.querySelector(".static-img_Id");
+  const lottieContainerId = document.getElementById("lottie-containerId");
+
+  const animation = lottie.loadAnimation({
+    container: lottieContainerId, // the dom element that will contain the animation
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    path: "https://lottie.host/9345c83a-eb88-457b-88b2-c980b5d3a492/g7LGrA8ItC.json", // the path to the animation json
+  });
+
+  animation.addEventListener("DOMLoaded", () => {
+    staticImageId.classList.add("hidden");
+    lottieContainerId.classList.remove("hidden");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const staticImageEn = document.querySelector(".static-img_En");
+  const lottieContainerEn = document.getElementById("lottie-containerEn");
+
+  const animation = lottie.loadAnimation({
+    container: lottieContainerEn, // the dom element that will contain the animation
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    path: "https://lottie.host/9054af9a-ccee-4ef1-b1ee-528c54889d7c/JgcZp5pin2.json", // the path to the animation json
+  });
+
+  animation.addEventListener("DOMLoaded", () => {
+    staticImageEn.classList.add("hidden");
+    lottieContainerEn.classList.remove("hidden");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const staticImageMobileId = document.querySelector(".static-img_MobileId");
+  const lottieContainerMobileId = document.getElementById(
+    "lottie-containerMobileId"
+  );
+
+  const animation = lottie.loadAnimation({
+    container: lottieContainerMobileId, // the dom element that will contain the animation
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    path: "https://lottie.host/8326d84d-d6a8-47f3-a3ed-d4e9da6bed05/ZyYK4WaDT7.json", // the path to the animation json
+  });
+
+  animation.addEventListener("DOMLoaded", () => {
+    staticImageMobileId.classList.add("hidden");
+    lottieContainerMobileId.classList.remove("hidden");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const staticImageMobileEn = document.querySelector(".static-img_MobileEn");
+  const lottieContainerMobileEn = document.getElementById(
+    "lottie-containerMobileEn"
+  );
+
+  const animation = lottie.loadAnimation({
+    container: lottieContainerMobileEn, // the dom element that will contain the animation
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    path: "https://lottie.host/1aa08efd-a7c3-4c15-879c-14b7c20e195d/KeMtFMi6qj.json", // the path to the animation json
+  });
+
+  animation.addEventListener("DOMLoaded", () => {
+    staticImageMobileEn.classList.add("hidden");
+    lottieContainerMobileEn.classList.remove("hidden");
+  });
+});
