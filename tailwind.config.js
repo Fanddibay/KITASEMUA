301e6dfc6b28c7b0d18c86c84489ffc02a022639
Fandi bayu <fanbayy@192.168.0.102>
2025-03-31 00:22:51 +0700

    upgrade to tailwindcss v-4

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
         "text-color": "#252525",
         "text-color-2": "#505050",
         "text-color-3": "#B5B5B5",
+        "primary-pink": "#D61679",
+        "secondary-pink": "#F916A8",
         "pink-hover": "#FF008A",
         "pink-custom": "#F916A8",
         "white-custom": "#E1E1E1",
